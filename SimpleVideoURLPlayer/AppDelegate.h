89b1d7032a33739c129951b18e9601f85799b5a5
Bobie <bobie@herxun.co>
2013-03-08 10:52:19 +0800

    Initial Commit

---
+++ SimpleVideoURLPlayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleVideoURLPlayer
+//
+//  Created by Bobie on 3/8/13.
+//  Copyright (c) 2013 Bobie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleVideoURLPlayer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SimpleVideoURLPlayer
+//
+//  Created by Bobie on 3/8/13.
+//  Copyright (c) 2013 Bobie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
